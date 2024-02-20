e424330d1b2d0d7a11698bc802274c6926e22113
EdizTeker <159573907+EdizTeker@users.noreply.github.com>
2024-02-20 15:14:01 +0300

    Add files via upload

---
+++ Constructors/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Constructors
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, FirstName = "Kaan", LastName = "Dere", City = "Rize" };
+            
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            //These two means the same thing and works the same way but written differently
+
+            
+            
+            Customer customer3 = new Customer (3, "Mehmet", "Demir", "İzmir");
+            //This one uses a constructor to assign value to the variables
+            Console.WriteLine(customer3.FirstName);
+
+
+        }
+    }
+
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+        public Customer(int id, string firstName, string lastName, string city) 
+        {
+            //Console.WriteLine("Constructor started");
+
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+
+
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
